8204f0ab4ef21e2dfa9cc3f5709bea122bb26585
adrake6 <aiden.drake@mail.mcgill.ca>
2020-06-17 11:38:10 -0400

    x axis works with mathplotlib now

--- python/DiffEqn.py
+++ python/DiffEqn.py
@@ -1,4 +1,4 @@
-## Differential Eqn Testing
+## Differential Eqn Testing -- won't be our final result
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 P = [498,]
 E = [1,]
 M = [1,]
+delta_t = 0.7
 
 def main():
-    global P, M, E
+    global P, M, E, delta_t
     
     phi = c = 0.1
     alpha = beta = 0.1
@@ -17,7 +18,6 @@ def main():
     rec = [[M[0], P[0], E[0]],]
     
     i = 1
-    delta_t = 0.7
     for t in np.arange(delta_t,150,delta_t):
         newP = (E[i-1]*phi-M[i-1]*P[i-1]*k*alpha)*delta_t+P[i-1]
         P.append(newP) ##P[i] = newP
@@ -28,20 +28,30 @@ def main():
         newM = (M[i-1]*P[i-1]*k*alpha-E[i-1]*M[i-1]*g*beta-M[i-1]*c)*delta_t+M[i-1]
         M.append(newM)
 
-        print(M[i], P[i], E[i])
+        ##print(M[i], P[i], E[i])
 
         rec.append([M[i], P[i], E[i]])
 
         i+=1
 
     ##recordToCsv(rec)
-    plot(rec)
+    plot()
         
-def plot(rec: 'list of lists'):
-    recarray = np.matrix(rec)
+def plot():
+    global P, E, M, delta_t
+
+    tScale = np.arange(0, 150, delta_t)
+    
+    PotsArr = np.asarray(P)
+    ExArr = np.asarray(E)
+    MemArr = np.asarray(M)
     
     fig, ax = plt.subplots()
-    ax.plot(recarray)
+    
+    ax.plot(tScale, PotsArr, label='Pots')
+    ax.plot(tScale, ExArr, label='Ex-mems')
+    ax.plot(tScale, MemArr, label='Mems')
+    ax.legend()
     plt.show()
     
         
